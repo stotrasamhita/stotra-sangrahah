5de4698dbaf7e546425f9c9c7da19676bc3473f0
karthikraman <karthik.raman@gmail.com>
2014-07-19 06:35:35 +0530

    Updated script
    tex2pdf now looks for any updated stotras and rebuilds PDFs.

--- stotras-pdf/tex2pdf.sh
+++ stotras-pdf/tex2pdf.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-#
-# Usage: find ../stotras -name "*.tex" | while read x; do ./tex2pdf.sh $x; done
-#
-fpath=$1
-fname=`basename $1`
+find ../stotras -name "*.tex" | while read fpath
+do
+fname=`basename $fpath`
 SSURL=`echo $fname | sed 's/.tex//;s/[A-Z]/_&/g' | sed 's/^_//;s/(_/_(/'`
-echo "StotraSamhita URL: $SSURL"
-echo "File path: $fpath"
+echo "---------------------------------------------------------------"
+echo "File path         : $fpath"
 jobname=`echo $fpath | sed 's/.tex//;s@.*stotras/@@'`
-echo "Job Name: $jobname"
+echo "PDF target        : $jobname.pdf"
+echo "StotraSamhita URL : $SSURL"
 mkdir -p `dirname $jobname`
+if [[ $fpath -nt $jobname.pdf ]]
+then
+echo Rebuilding $jobname.pdf... > /dev/stderr
+echo Rebuilding $jobname.pdf...
 cat stotra-template.tex | sed "s@FPATH@$fpath@;s@SSURL@$SSURL@" | xelatex -jobname=$jobname
+else
+echo PDF up-to-date.
+fi
+done > tex2pdf.log
