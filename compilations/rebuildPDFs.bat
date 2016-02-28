d6db86558e58b0e085da9beb25b4e1fc62ff6f2d
Karthik Raman <karthik.raman@gmail.com>
2016-02-28 08:55:35 +0530

    Fixed SuryaMandalaStotram and updated PDFs

---
+++ compilations/rebuildPDFs.bat
@@ -0,0 +1,9 @@
+cd stotramanjari
+latexmk -xelatex stotramanjari-1 -g >> rebuild.log
+latexmk -xelatex stotramanjari-2 -g >> rebuild.log
+latexmk -xelatex stotramanjari-3 -g >> rebuild.log
+latexmk -xelatex stotramanjari -g >> rebuild.log
+cd ..
+cd nityaparayanam
+latexmk -xelatex nityaparayanam -g >> rebuild.log
+cd ..
