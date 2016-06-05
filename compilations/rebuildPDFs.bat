37259ab0a682aff93657f972281f46869e2516f5
Karthik Raman <karthik.raman@gmail.com>
2016-06-05 11:40:22 +0530

    pdfs rebuilt

--- compilations/rebuildPDFs.bat
+++ compilations/rebuildPDFs.bat
@@ -3,8 +3,10 @@ latexmk -xelatex stotramanjari-1 -g >> rebuild.log
 latexmk -xelatex stotramanjari-2 -g >> rebuild.log
 latexmk -xelatex stotramanjari-3 -g >> rebuild.log
 latexmk -xelatex stotramanjari -g >> rebuild.log
+latexmk -xelatex stotramanjari-rm -g >> rebuild.log
 cd ..
 cd nityaparayanam
 latexmk -xelatex nityaparayanam -g >> rebuild.log
 latexmk -xelatex nityaparayanam-vedam -g >> rebuild.log
+latexmk -xelatex mantrapushpam-mini -g >> rebuild.log
 cd ..
