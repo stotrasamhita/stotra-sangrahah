da95e666dd3a4c03e65165a6ba025c497b68fb76
Karthik Raman <karthik.raman@gmail.com>
2018-01-10 11:48:18 +0530

    overfull hboxes fixed

--- stotras-pdf/tex2pdf.sh
+++ stotras-pdf/tex2pdf.sh
@@ -5,7 +5,7 @@ fname=`basename $fpath`
 SSURL=`echo $fname | sed 's/.tex//;s/[A-Z]/_&/g' | sed 's/^_//;s/(_/_(/'`
 echo "---------------------------------------------------------------"
 echo "File path         : $fpath"
-jobname=`echo $fpath | sed 's/.tex//;s@.*stotras/@@'`
+jobname=`echo $fpath | sed 's/.tex//;s@../stotras/@@'`
 echo "PDF target        : $jobname.pdf"
 echo "StotraSamhita URL : $SSURL"
 mkdir -p `dirname $jobname`
